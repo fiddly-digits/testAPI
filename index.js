206ef1a04eebccec00ca4205e1df9b0c4e88caa0
fiddly-digits <rob.crlu@gmail.com>
2023-06-06 19:54:40 -0600

    Exercise of Mentors with Query and Path Params

--- index.js
+++ index.js
@@ -17,7 +17,14 @@ app.get('/', (req, res) => {
 app.get('/koders', async (req, res) => {
   const db = await fsProm.readFile('./koders.json', 'utf8');
   const dbParsed = JSON.parse(db); // * Parseamos el JSON
-  res.json(dbParsed); //* Regresamos un header de content-type -> application/json
+  const { module } = req.query;
+  const selectedKoder = dbParsed.koders.reduce((accum, current) => {
+    if (current.module.toLowerCase() === module.toLowerCase()) {
+      accum = { ...current };
+    }
+    return accum;
+  }, {});
+  res.json(selectedKoder); //* Regresamos un header de content-type -> application/json
 });
 
 /*
@@ -27,6 +34,15 @@ app.get('/koders', async (req, res) => {
  * /route/:pathParam
  */
 
+/*
+app.get('/koders', async (req, res) => {
+  const db = await fsProm.readFile('./koders.json', 'utf8');
+  const dbParsed = JSON.parse(db); // * Parseamos el JSON
+  res.json(dbParsed.koders); //* Regresamos un header de content-type -> application/json
+});
+*/
+
+// ! Path Param
 app.get('/koders/:name', async (req, res) => {
   const db = await fsProm.readFile('./koders.json', 'utf8');
   const dbParsed = JSON.parse(db);
@@ -38,6 +54,60 @@ app.get('/koders/:name', async (req, res) => {
     return accum;
   }, {});
   res.json(selectedKoder);
+});
+
+// ! Query Params
+
+app.get('/koders', async (req, res) => {
+  const db = await fsProm.readFile('./koders.json', 'utf8');
+  const dbParsed = JSON.parse(db);
+  const { module } = req.query;
+  console.log('current: ', current.module);
+  console.log('Query', module);
+  const selectedKoder = dbParsed.koders.reduce((accum, current) => {
+    if (current.module.toLowerCase() === module.toLowerCase()) {
+      accum = { ...current };
+    }
+    return accum;
+  }, {});
+  res.json(selectedKoder);
+});
+
+/**
+ * Ejercicio
+ * 2 endpoints -
+ * 1.er Donde me filtren por age los mentores
+ * 2.do Donde obtengamos un mentor en especifico con su nombre
+ */
+
+// ! Mentors Query Param
+
+app.get('/mentors', async (req, res) => {
+  const db = await fsProm.readFile('./koders.json', 'utf8');
+  const dbParsed = JSON.parse(db);
+  const { age } = req.query;
+  const selectedMentor = dbParsed.mentors.reduce((accum, current) => {
+    if (current.age === age) {
+      accum = { ...current };
+    }
+    return accum;
+  }, {});
+  selectedMentor ? res.json(selectedMentor) : res.json(dbParsed.mentors);
+});
+
+// ! Mentors Path Param
+
+app.get('/mentors/:name', async (req, res) => {
+  const db = await fsProm.readFile('./koders.json', 'utf8');
+  const dbParsed = JSON.parse(db);
+  const { name } = req.params;
+  const selectedMentor = dbParsed.mentors.reduce((accum, current) => {
+    if (current.name.toLowerCase() === name.toLowerCase()) {
+      accum = { ...current };
+    }
+    return accum;
+  }, {});
+  res.json(selectedMentor);
 });
 
 app.listen(8080, () => {
